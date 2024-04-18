fab36519b16520270c462cbca253ca983701e0d3
AHAMMAD ASLAM NAVAS <142337596+devahammad@users.noreply.github.com>
2024-04-18 14:00:40 +0400

    Update second.js

--- second.js
+++ second.js
@@ -1,45 +1,15 @@
-$(document).ready(function() {
-  $('#contact-form').submit(function(event) {
-      event.preventDefault(); // Prevent the default form submission
-      
-      var username = $('#username').val();
-      var email = $('#email').val();
-      var message = $('#message').val();
-  
-      var messageToTelegram = "**New Enquiry**\n\n" +
-          "Name: " + username + "\n\n" +
-          "Email: " + email + "\n\n" +
-          "Message: " + message;
-  
-     // Replace with your Telegram bot API endpoint
-     var telegramApiUrl = 'https://api.telegram.org/bot7000835701:AAFZfPlO7R9n55pSORl1iIDWh29AVjHOZBo/sendMessage';
-  
-     // Replace '<YourChatID>' with your actual chat ID
-     var chatId = '5405301126';
-  
-      // Send the message to Telegram using $.ajax
-      $.ajax({
-          url: telegramApiUrl,
-          method: 'POST',
-          data: {
-              chat_id: chatId,
-              text: messageToTelegram
-          },
-          success: function(data) {
-              if (data.ok) {
-                  // Clear form fields
-                  $('#username').val('');
-                  $('#email').val('');
-                  $('#message').val('');
-              } else {
-                  showAlert('Submission failed.');
-              }
-          },
-      });
-  });
-
-  function showAlert(message) {
-      // Display alert message
-      alert(message);
-  }
+$("#submit-form").submit((e) => {
+    e.preventDefault();
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbwW4MXsRvIqU1K5Ws4vxcpEfPMRAI9Qy1o3kVQiImxj8VVY2K2nfYI29blK9OZy2dg/exec",
+        data: $("#submit-form").serialize(),
+        method: "post",
+        success: function(response) {
+            $("#submit-form")[0].reset();
+        },
+        error: function(err) {
+            alert("Something Error");
+        }
+    });
 });
+
