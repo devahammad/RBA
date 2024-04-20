d56f6ff42beb97ef10e7a40ba9e9a587221667c4
AHAMMAD ASLAM NAVAS <142337596+devahammad@users.noreply.github.com>
2024-04-20 13:08:15 +0400

    Update second.js

--- second.js
+++ second.js
@@ -13,21 +13,6 @@ $("#submit-form").submit((e) => {
     });
 });
 
-$("#submit-form").submit((e) => {
-    e.preventDefault();
-    $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbwW4MXsRvIqU1K5Ws4vxcpEfPMRAI9Qy1o3kVQiImxj8VVY2K2nfYI29blK9OZy2dg/exec",
-        data: $("#submit-form").serialize(),
-        method: "post",
-        success: function(response) {
-            $("#submit-form")[0].reset();
-        },
-        error: function(err) {
-            alert("Something Error");
-        }
-    });
-});
-
 
 document.addEventListener('DOMContentLoaded', function () {
     const formParent = document.querySelector('.parentSupport');
@@ -49,3 +34,4 @@ document.addEventListener('DOMContentLoaded', function () {
         thankYouDiv.style.display = 'flex';
     });
 });
+
